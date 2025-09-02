224d720b0075c8dc165c27e1ef635331d278cce8
Shan Pen <bricle031@gmail.com>
2025-09-02 22:35:22 +0800

    drivers: airoc: add default init priority configuration for AIROC Wi-Fi drivers
    Signed-off-by: Shan Pen <bricle031@gmail.com>

--- drivers/wifi/infineon/airoc_wifi.c
+++ drivers/wifi/infineon/airoc_wifi.c
@@ -878,7 +878,7 @@ static const struct net_wifi_mgmt_offload airoc_api = {
 };
 
 NET_DEVICE_DT_INST_DEFINE(0, airoc_init, NULL, &airoc_wifi_data, &airoc_wifi_config,
-			  CONFIG_WIFI_INIT_PRIORITY, &airoc_api, ETHERNET_L2,
+			  CONFIG_AIROC_WIFI_INIT_PRIORITY, &airoc_api, ETHERNET_L2,
 			  NET_L2_GET_CTX_TYPE(ETHERNET_L2), WHD_LINK_MTU);
 
 CONNECTIVITY_WIFI_MGMT_BIND(Z_DEVICE_DT_DEV_ID(DT_DRV_INST(0)));
